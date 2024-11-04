770ba7c31aad4a979fd81b24a67761f3c6e3da3e
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2024-11-04 21:57:14 +0530

    Time: 31 ms (64.96%), Space: 29.5 MB (74.87%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string compressedString(string s) {
         int count = 1;
-        string res = "";
+        stringstream res;
         for (int i = 0; i < s.length(); i++) {
             if (i < s.length() - 1 && s[i] == s[i + 1] && count < 9) {
                 count++;
             } else {
-                res += to_string(count) + s[i];
+                res << count << s[i];
                 count = 1;
             }
         }
-        return res;
+        return res.str();
     }
 };
