bea07b5a72a3c71a53dd08d60873f6544b5b9e81
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2024-11-13 22:25:21 +0530

    Time: 68 ms (41.07%), Space: 60.3 MB (61.66%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -6,11 +6,10 @@ public:
         long long count = 0;
 
         for (int i = 0; i < n - 1; ++i) {
-            // Find the lower bound for j where nums[i] + nums[j] >= lower
-            int low_j = std::lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+     
+            int low_j = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
             
-            // Find the upper bound for j where nums[i] + nums[j] <= upper
-            int high_j = std::upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+            int high_j = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
             
             count += high_j - low_j;
         }
