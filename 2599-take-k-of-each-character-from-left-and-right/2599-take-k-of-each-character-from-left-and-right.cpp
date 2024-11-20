9249c32d3cbf0fa78b3a2883f04c937635643c0f
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2024-11-20 23:18:45 +0530

    Time: 6 ms (91.71%), Space: 12.2 MB (56.21%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int n=s.length();
-        vector<int>freq(3,0);
-        for(auto ch:s){
-            freq[ch-'a']++;
+        int n = s.length();
+        int count_a =0;
+        int count_b =0;
+        int count_c=0;
+        for(char &ch:s){
+            if(ch=='a'){
+                count_a++;
+            }
+            else if(ch=='b'){
+                count_b++;
+            }
+            else if(ch=='c'){
+                count_c++;
+            }
         }
-        if(freq[0]<k || freq[1]<k || freq[2]<k) return -1;
-
-        int ans=0;
+        if(count_a<k|| count_b<k||count_c<k){
+            return -1;
+        }
         int i=0;
         int j=0;
-        while(i<n && j<n){
-            freq[s[j]-'a']--;
-            j++;
-            while(freq[0]<k || freq[1]<k || freq[2]<k){
-                freq[s[i]-'a']++;
+        int NotdeletedWindowSize=0;
+        while(j<n){
+            if(s[j]=='a'){
+                count_a--;
+            }else if(s[j]=='b'){
+                count_b--;
+            }else if(s[j]=='c'){
+                count_c--;
+            }
+            while(i<=j && (count_a<k || count_b<k||count_c<k)){
+                if(s[i]=='a'){
+                    count_a++;
+                }else if(s[i]=='b'){
+                count_b++;
+                }else{
+                    count_c++;
+                }
                 i++;
             }
-            if(freq[0]>=k && freq[1]>=k && freq[2]>=k) ans=max(ans,j-i+1);
+            NotdeletedWindowSize = max(NotdeletedWindowSize,j-i+1);
+            j++;
         }
-        return n-ans+1;
+        return n-NotdeletedWindowSize;
     }
 };
