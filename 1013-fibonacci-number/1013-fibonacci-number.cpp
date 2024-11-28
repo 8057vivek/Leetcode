22f898c9883bc22f5743c5432e0025fbd604f4b7
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2024-11-28 21:04:41 +0530

    Time: 0 ms (100%), Space: 8.1 MB (11.22%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     
+    unordered_map<int,int> memo;
     int fib(int n) {
+        if(memo.find(n)!=memo.end()){
+            return memo[n];
+        }
         if(n==0){
             return 0;
         }
@@ -9,6 +13,7 @@ public:
             return 1;
         }
         int res = fib(n-1)+fib(n-2);
+        memo[n]=res;
 
         return res;
     }
