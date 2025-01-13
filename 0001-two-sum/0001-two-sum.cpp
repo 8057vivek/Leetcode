bc370ce01af7029d8dc8b5163c0a4bec79928646
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-01-14 00:00:05 +0530

    Time: 11 ms (47.2%), Space: 15.8 MB (9.73%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        unordered_map<int,int> map;
+
+        for(int i=0;i<n;i++){
+           map[nums[i]] = i;
+        }
+        vector<int> result;
+        for(int i=0;i<n;i++){
+            int Val = target-nums[i];
+            if(map.find(Val)!=map.end() && map[Val] != i){
+                result.push_back(i);
+                result.push_back(map[Val]);
+                return result;
+            }
+        }
+
+        return result;
+    }
+};
