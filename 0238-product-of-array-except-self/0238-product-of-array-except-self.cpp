2cea2408c5665fa45ef9c72c500233103a21782a
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-01-14 11:04:29 +0530

    Time: 4 ms (23.44%), Space: 40.2 MB (55.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,20 +3,26 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> prefix(n,1);
-        vector<int> suffix(n,1);
+        // vector<int> prefix(n,1);
+        // vector<int> suffix(n,1);
 
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=nums[i-1]*prefix[i-1];
+        // }
+
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i]=nums[i+1]*suffix[i+1];
+        // }
+
+        vector<int> result(n,1);
         for(int i=1;i<n;i++){
-            prefix[i]=nums[i-1]*prefix[i-1];
+            result[i]=nums[i-1]*result[i-1];
         }
 
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=nums[i+1]*suffix[i+1];
-        }
-
-        vector<int> result(n);
-        for(int i=0;i<n;i++){
-            result[i]=prefix[i]*suffix[i];
+        int right = 1;
+        for(int i=n-1;i>=0;i--){
+            result[i]=result[i]*right;
+            right *= nums[i];
         }
 
         return result;
