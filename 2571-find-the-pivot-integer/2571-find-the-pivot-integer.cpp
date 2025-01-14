eee418cd8fc84a6c2360b3a585fe533046798756
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-01-14 21:47:04 +0530

    Time: 0 ms (100%), Space: 8 MB (59.79%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.cpp
@@ -2,30 +2,15 @@ class Solution {
 public:
     int pivotInteger(int n) {
         
-        vector<int> arr(n);
-        for(int i=0;i<n;i++){
-            arr[i]=i+1;
-        }
+        int totalSum = (n*(n+1))/2;
+        int prefixSum = 0;
 
-        vector<int> prefix(n);
-        vector<int> suffix(n);
-
-        prefix[0]=arr[0];
-
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+arr[i];
-        }
-        
-        suffix[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            suffix[i]=suffix[i+1]+arr[i];
-        }
-        
-        for(int i=0;i<n;i++){
-            if(prefix[i]==suffix[i]){
-                return i+1;
+        for(int i=1;i<=n;i++){
+            prefixSum+=i;
+            if(prefixSum==totalSum){
+                return i;
             }
+            totalSum-=i;
         }
         
         return -1;
