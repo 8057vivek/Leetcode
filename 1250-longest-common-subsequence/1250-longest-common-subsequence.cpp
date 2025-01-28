f9e3c71b2676cc1c53da02b9d3fac6a6f4b7d25a
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-01-28 10:41:03 +0530

    Time: 11 ms (98.07%), Space: 9.2 MB (88.46%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,19 +3,21 @@ public:
     int tabulation(string text1, string text2){
         int m = text1.length();
         int n = text2.length();
-        vector<vector<int>> table(m+1,vector<int>(n+1,0));
-        
-        for(int i=m-1;i>=0;i--){
-            for(int j = n-1;j>=0;j--){
-                if(text1[i]==text2[j]){
-                    table[i][j] = 1+table[i+1][j+1];
-                }
-                else{
-                    table[i][j] = max(table[i+1][j], table[i][j+1]);
+
+        vector<int> curr(n + 1, 0), next(n + 1, 0);
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (text1[i] == text2[j]) {
+                    curr[j] = 1 + next[j + 1];
+                } else {
+                    curr[j] = max(next[j], curr[j + 1]);
                 }
             }
+            next = curr;
         }
-        return table[0][0];
+
+        return next[0];
     }
     int longestCommonSubsequence(string text1, string text2) {
         return tabulation(text1,text2);
