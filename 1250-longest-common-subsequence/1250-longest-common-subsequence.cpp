d68e28251e1750d203a0fbacbbbceb03bce96023
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-01-28 09:57:09 +0530

    Time: 43 ms (23.58%), Space: 27.6 MB (15.19%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int recursiveFunction(int i, int j, string& s1, string& s2,vector<vector<int>>& dp){
-        if(i<0 || j<0){
+        if(i>=s1.length() || j>=s2.length()){
             return 0;
         }
         if(dp[i][j]!=-1){
@@ -9,16 +9,16 @@ public:
         }
         int ans = 0;
         if(s1[i]==s2[j]){
-            ans = 1+ recursiveFunction(i-1,j-1,s1,s2,dp);
+            ans = 1+ recursiveFunction(i+1,j+1,s1,s2,dp);
         }else{
-            ans = max(recursiveFunction(i-1,j,s1,s2,dp),recursiveFunction(i,j-1,s1,s2,dp));
+            ans = max(recursiveFunction(i+1,j,s1,s2,dp),recursiveFunction(i,j+1,s1,s2,dp));
         }
         return dp[i][j]=ans;
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return recursiveFunction(n - 1, m - 1, text1, text2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return recursiveFunction(0, 0, text1, text2, dp);
     }
 };
