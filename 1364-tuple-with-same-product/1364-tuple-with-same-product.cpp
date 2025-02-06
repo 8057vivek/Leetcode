78b83f59eefbfcb04505fd1957135966e701954d
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-02-06 21:10:09 +0530

    Time: 370 ms (21.63%), Space: 84.9 MB (81.5%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> productCount;
+        int n = nums.size();
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int product = nums[i] * nums[j];
+                result += productCount[product] * 8; 
+                productCount[product]++;
+            }
+        }
+        return result;
+    }
+};
