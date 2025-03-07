0cc605149753d404ec2266b6c892e6beb887cb1e
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-07 18:41:43 +0530

    Time: 1092 ms (5.18%), Space: 17.4 MB (68.7%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPrime(int num){
+        if(num<2){
+            return false;
+        }
+        for(int i=2;i<=sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<int> closestPrimes(int left, int right) {
+        vector<int> vec;
+
+        for(int i=left;i<=right;i++){
+            if(isPrime(i)){
+                vec.push_back(i);
+            }
+        }
+        if (vec.size() < 2) return {-1, -1};
+        vector<int> result(2, -1);
+
+        int minDiff = INT_MAX;
+        int n = vec.size();
+        for(int i=1;i<n;i++){
+            int diff = vec[i]-vec[i-1];
+            if(diff< minDiff){
+                minDiff = diff;
+                result[0] = vec[i-1];
+                result[1] = vec[i];
+            }
+        }
+
+        return result;
+    }
+};
