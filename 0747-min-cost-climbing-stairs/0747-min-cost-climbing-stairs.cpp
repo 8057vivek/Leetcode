050fb8b44a484bb66cdcac7350e214726b64ab76
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-08 14:15:25 +0530

    Time: 0 ms (100%), Space: 17.6 MB (51.08%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        
-        cost.push_back(0);
-        int n = cost.size();
-
-        for(int i=n-3;i>=0;i--){
-            cost[i]=min(cost[i]+cost[i+2], cost[i]+cost[i+1]);
+    int dp[1001];
+    int recursion(vector<int>& cost, int index, int n){
+        if(index>=n){
+            return 0;
         }
-        
-        return min(cost[0], cost[1]);
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        int oneStep = cost[index] +  recursion(cost, index+1,n);
+        int TwoStep = cost[index] + recursion(cost, index+2,n);
 
+        return dp[index]=min(oneStep, TwoStep);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        memset(dp, -1, sizeof(dp));
+        int n = cost.size();
+        return min(recursion(cost, 0,n), recursion(cost, 1,n));
     }
 };
