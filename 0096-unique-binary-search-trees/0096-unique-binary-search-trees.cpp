8698ae76421c5b7b9f17ba56b6aa5b694a1fe3e8
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-10 11:29:03 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.84%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int recursion(int n){
+    int recursion(int n,vector<int>& dp){
         if(n<=1){
             return 1;
         }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
         int count=0;
         for(int i=1;i<=n;i++){
-            count+=recursion(i-1)*recursion(n-i);
+            count+=recursion(i-1,dp)*recursion(n-i,dp);
         }
-        return count;
+        return dp[n] = count;
     }
     int numTrees(int n) {
-        return recursion(n);
+        vector<int> dp(n+1,-1);
+        return recursion(n,dp);
     }
 };
