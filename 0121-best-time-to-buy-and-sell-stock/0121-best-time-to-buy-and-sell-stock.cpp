dc8a9c285c5c2e9eadffe549907ce1e27949eb5e
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-10 10:46:01 +0530

    Time: 8 ms (6.76%), Space: 107.6 MB (5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int recursion(vector<int>& prices,int minPrice,  int index){
+    int recursion(vector<int>& prices,int minPrice,  int index,vector<int>& dp){
         if(index>=prices.size()){
             return 0;
         }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+
         int newMinPrice = min(minPrice, prices[index]);
         int profit = prices[index]-minPrice;
-        int nextProfit = recursion(prices, newMinPrice, index+1);
+        int nextProfit = recursion(prices, newMinPrice, index+1,dp);
 
-        return max(profit, nextProfit);
+        return dp[index]= max(profit, nextProfit);
 
     }
     int maxProfit(vector<int>& prices) {
-        return recursion(prices, prices[0], 0);
+        int n = prices.size();
+        vector<int>dp (n,-1);
+        return recursion(prices, prices[0], 0,dp);
     }
 };
