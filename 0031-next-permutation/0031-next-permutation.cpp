84d9c7debe388c038c43a39547fe76368317c65a
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-13 11:03:35 +0530

    Time: 0 ms (100%), Space: 15.7 MB (68.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(), i = n - 2;
-        
-        // Step 1: Find the first decreasing element from the right
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+
+        int n = nums.size();
+        if(n<=1){
+            return;
+        }
+        int i = n-2;
+
+        while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
-
-        if (i >= 0) {
-            // Step 2: Find the next greater element to swap
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
+        
+        if(i>=0){
+            int j=n-1;
+            while(nums[j]<=nums[i]){
                 j--;
             }
-            swap(nums[i], nums[j]);
+            swap(nums[i],nums[j]);
         }
+        reverse(nums.begin()+i+1,nums.end());
 
-        // Step 3: Reverse the remaining part
-        reverse(nums.begin() + i + 1, nums.end());
     }
 };
