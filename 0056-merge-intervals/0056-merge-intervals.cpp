2c2632dea761083ddffc43cd11ea81e9575225a1
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-25 23:26:58 +0530

    Time: 4 ms (70.85%), Space: 23.9 MB (59.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> result;
+
         sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
         result.push_back(intervals[0]);
-        int first = intervals[0][0];
-        int second = intervals[0][1];
 
-        int size = 0;
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= result.back()[1]) {
 
-        int n = intervals.size();
-        for(int i=1;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(start<=second){
-                // result[size][0]=min(first, start);
-                result[size][1]=max(second, end);
-                first = result[size][0];
-                second = result[size][1];
-                // vector<int> vec;
-                // vec.push_back(min(first, start));
-                // vec.push_back(max(second, end));
-                // result.push
-            }else{
-            vector<int> vec;
-            vec.push_back(start);
-            vec.push_back(end);
-            result.push_back(vec);
-            first = start;
-            second = end;
-            size++; 
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            } else {
+                
+                result.emplace_back(intervals[i]);
             }
         }
-
         return result;
     }
 };
