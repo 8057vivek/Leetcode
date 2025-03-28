119535e11d020833c7f1530ce4a1d6bb005e8024
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-03-28 22:06:25 +0530

    Time: 53 ms (40.62%), Space: 69.1 MB (68.37%) - LeetHub

--- 0922-possible-bipartition/0922-possible-bipartition.cpp
+++ 0922-possible-bipartition/0922-possible-bipartition.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    bool bfs(vector<vector<int>>& graph, int start, vector<int>& color) {
-        queue<int> q;
-        q.push(start);
-        color[start] = 0;
+    bool bfs(int node, vector<vector<int>> & adj, vector<int> & color, int parentColor){
+        queue<pair<int, int>> q;
+        color[node] = parentColor;
+        q.push({node, parentColor});
 
-        while (!q.empty()) {
-            int node = q.front();
+        while(!q.empty()){
+            int currNode = q.front().first;
+            int  currColor = q.front().second;
             q.pop();
 
-            for (int neighbor : graph[node]) {
-                if (color[neighbor] == -1) { 
-                    color[neighbor] = 1 - color[node];
-                    q.push(neighbor);
-                } else if (color[neighbor] == color[node]) {
-                    return false; 
+            for(int nbr : adj[currNode]){
+                if(color[nbr]==-1){
+                    color[nbr]=1-currColor;
+                    q.push({nbr, color[nbr]});
+                }
+                else if(color[nbr]==currColor){
+                    return false;
                 }
             }
         }
+
         return true;
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<vector<int>> adj(n+1);
-        for(vector<int>& vec : dislikes){
-            adj[vec[0]].push_back(vec[1]);
-            adj[vec[1]].push_back(vec[0]);
-        }
-        vector<int> color(n+1, -1); 
 
-        for (int i = 1; i <= n; i++) {
-            if (color[i] == -1) { 
-                if (!bfs(adj, i, color)) {
+        for(vector<int>& vec : dislikes){
+            int u = vec[0];
+            int v = vec[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> color(n+1, -1);
+        for(int i=1;i<=n;i++){
+            if(color[i]==-1){
+                if(!bfs(i, adj, color, 1)){
                     return false;
                 }
             }
