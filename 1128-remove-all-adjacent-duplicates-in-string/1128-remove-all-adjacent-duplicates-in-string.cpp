c61242b1f546a1b75e79c77270eb12d156f072e8
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-05-14 22:13:59 +0530

    Time: 7 ms (50.34%), Space: 14.4 MB (29.8%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,18 +4,19 @@ public:
         int n = s.length();
         stack<char> st;
         for(int i=0;i<n;i++){
-            if(!st.empty() && st.top()==s[i]){
-                st.pop();
-            }else{
+            if(st.empty() || st.top()!=s[i]){
                 st.push(s[i]);
+            }else{
+                st.pop();
             }
         }
         string res = "";
         while(!st.empty()){
-            res = st.top() + res;
+            res.push_back(st.top());
             st.pop();
         }
 
-        return res; 
+        reverse(res.begin(), res.end()); 
+        return res;
     }
 };
