3f1d8e238706d3f6ed9216977fb59637e7af4b64
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-05-26 19:46:13 +0530

    Time: 0 ms (100%), Space: 12.2 MB (53.06%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -4,14 +4,30 @@ public:
         int n = boxes.size();
         vector<int> result(n,0);
 
+        // for(int i=0;i<n;i++){
+        //     int stepsToAdd = 0;
+        //     for(int j=0;j<n;j++){
+        //         if(boxes[j]=='1'){
+        //             stepsToAdd += abs(i-j);
+        //         }
+        //     }
+        //     result[i] = stepsToAdd;
+        // }
+
+        int balls = 0;
+        int stepsToAdd = 0;
         for(int i=0;i<n;i++){
-            int stepsToAdd = 0;
-            for(int j=0;j<n;j++){
-                if(boxes[j]=='1'){
-                    stepsToAdd += abs(i-j);
-                }
-            }
-            result[i] = stepsToAdd;
+            result[i] += stepsToAdd;
+            balls += boxes[i]-'0';
+            stepsToAdd += balls;
+        }
+        
+        balls = 0;
+        stepsToAdd = 0;
+        for(int i=n-1;i>=0;i--){
+            result[i] += stepsToAdd;
+            balls += boxes[i]-'0';
+            stepsToAdd += balls;
         }
 
         return result;
