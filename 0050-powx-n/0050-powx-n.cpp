2fbcfbe9e7ca39c24dcd493534628beae2b10702
vivek baghel <112117888+8057vivek@users.noreply.github.com>
2025-06-23 21:55:49 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.29%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        double result = 1.0;
+        long exponent = n;
+
+        if (exponent < 0) {
+            exponent = -exponent;
+            x = 1 / x;
+        }
+
+        while (exponent > 0) {
+            if (exponent % 2 == 1)
+                result *= x;
+            x *= x;
+            exponent /= 2;
+        }
+
+        return result;
     }
 };
