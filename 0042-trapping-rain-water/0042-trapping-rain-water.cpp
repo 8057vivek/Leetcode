7da71abaf5ad477944003023cf7043d252cd9cca
Vivek Baghel <112117888+8057vivek@users.noreply.github.com>
2025-07-23 22:11:50 +0530

    Time: 0 ms (100%), Space: 27.8 MB (5.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int left = 0, right=n-1;
-        int maxLeft = 0, maxRight = 0, water=0;
-
-        while(left<right){
-            if(height[left]<height[right]){
-                maxLeft = max(maxLeft, height[left]);
-                water += maxLeft-height[left];
-                left++;
-            }
-            else{
-                maxRight = max(maxRight, height[right]);
-                water += maxRight-height[right];
-                right--;
-            }
+        if(n<=2){
+            return 0;
         }
+
+        vector<int> prefixWater;
+        vector<int> SuffixWater(n-2);
+
+        int maxLeft = height[0];
+        int maxRight = height[n-1];
+
+        int water = 0;
+
+        for(int i=1;i<n-1;i++){
+            maxLeft = max(maxLeft, height[i]);
+            prefixWater.push_back(maxLeft-height[i]);
+        }
+
+        for(int i=n-2;i>0;i--){
+            int j=n-2;
+            maxRight = max(maxRight, height[i]);
+            SuffixWater[i-1]=maxRight-height[i];
+        }
+
+        for(int i=0;i<prefixWater.size();i++){
+            water += min(prefixWater[i],SuffixWater[i]);
+        }
+
         return water;
     }
 };
