de1713dd70d407b70e0942abda816442e1317b65
Vivek Baghel <112117888+8057vivek@users.noreply.github.com>
2025-08-30 22:31:10 +0530

    Time: 0 ms (100%), Space: 21.6 MB (91.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<string> seen;
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char num = board[i][j];
-                if (num == '.') continue;
-
-                string row = to_string(i) + " in row " + num;
-                string col = to_string(j) + " in col " + num;
-                string box = to_string(i / 3) + to_string(j / 3) + " in box " + num;
-
-                if (seen.count(row) || seen.count(col) || seen.count(box))
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char num=board[i][j];
+                if(num!='.' && isSafe(board,i,j,num)==false){
                     return false;
+                }
+            }
+        }
 
-                seen.insert(row);
-                seen.insert(col);
-                seen.insert(box);
+        return true;
+    }
+    bool isSafe(vector<vector<char>>& board,int row,int col,char num){
+        // row check
+        for(int i=0;i<9;i++){
+            if(i!=col && board[row][i]==num){
+                return false;
+            }
+        }
+        //col check
+        for(int i=0;i<9;i++){
+            if(i!=row && board[i][col]==num){
+                return false;
+            }
+        }
+        //grid check
+        int row_=(row/3)*3;
+        int col_=(col/3)*3;
+        for(int i=row_;i<row_+3;i++){
+            for(int j=col_;j<col_+3;j++){
+                if((i!=row || j!=col) && board[i][j]==num){
+                    return false;
+                }
+                
             }
         }
         return true;
