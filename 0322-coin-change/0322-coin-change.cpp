605293f3010bbd2fc8fbb432625dce6e3c9f4be4
Vivek Baghel <112117888+8057vivek@users.noreply.github.com>
2025-09-09 21:01:55 +0530

    Time: 51 ms (25.61%), Space: 49.7 MB (22.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, int index, int n, vector<vector<int>>& dp){
+    int recursion(int index, vector<int>& coins, int amount,vector<vector<int>>& dp){
         if(amount==0){
             return 0;
         }
-
-        if(index>=n){
-            return INT_MAX-1;
+        if(index<0){
+            return 1e9;
         }
         if(dp[index][amount]!=-1){
             return dp[index][amount];
         }
-        
-        int  take= INT_MAX-1;
-        if(coins[index]<=amount){
-            take = 1+solve(coins, amount-coins[index], index, n, dp);
-        }
-        int notTake = solve(coins, amount, index+1, n,dp);
 
-        return dp[index][amount]=min(take, notTake);
+        int take = 1e9;
+        if(coins[index]<=amount){
+            take = 1+recursion(index, coins, amount-coins[index],dp);
+        }
+        int notTake = recursion(index-1, coins, amount,dp);
+
+        return dp[index][amount] = min(take, notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
+
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        int result = solve(coins, amount, 0, n,dp);
-        if(result==INT_MAX-1){
+        
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int res = recursion(n-1, coins, amount,dp);
+        if(res==1e9){
             return -1;
         }
-        return result;
+        return res;
     }
 };
