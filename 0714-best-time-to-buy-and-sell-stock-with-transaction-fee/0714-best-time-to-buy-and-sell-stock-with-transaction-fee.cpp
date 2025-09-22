58a88e58b0280464cc50369f044143717b4b173a
Vivek Baghel <112117888+8057vivek@users.noreply.github.com>
2025-09-22 15:43:14 +0530

    Time: 115 ms (27.48%), Space: 100.1 MB (33.28%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public: 
+    int recursion(int index, int buy, int fee, vector<int>& prices, vector<vector<int>> & dp){
+        if(index == prices.size()){
+            return 0;
+        }
+
+        if(dp[index][buy]!=-1){
+            return dp[index][buy];
+        }
+
+        int profit = 0;
+        if(buy){
+            profit = max(-prices[index]+recursion(index+1, 0, fee, prices, dp),
+                        0+recursion(index+1, 1, fee, prices, dp));
+        }
+        else{
+            profit = max(-fee + prices[index] + recursion(index+1, 1, fee, prices, dp),
+                         0+recursion(index+1,0, fee, prices, dp));
+        }
+        return dp[index][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2 , -1));
+
+        return recursion(0, 1, fee, prices, dp);
+    }
+};
